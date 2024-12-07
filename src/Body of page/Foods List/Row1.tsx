f5b77dbf8f566ec31a363273458f480308686b26
mmd-rahimi <mohamad8rahimi3@gmail.com>
2024-12-07 09:16:16 +0330

    creating food box

--- src/Body of page/Foods List/Row1.tsx
+++ src/Body of page/Foods List/Row1.tsx
@@ -1,9 +1,19 @@
 export function Row1() {
     const flexFoods =
     <div className="flex flex-row gap-5 mt-5 justify-center">
-        <div className="bg-black">1</div>
+        <div className="bg-white w-[22rem] h-[18rem] rounded-xl">
+            <div className="w-[20rem] h-[9rem] mx-auto mt-4">
+                <img className="h-full w-full" src="./public/burger.jpg" alt="" />
+                <p className="font-medium text-lg mt-2">چیز برگر مرددد</p>
+                <p className="font-normal text-sm mt-2 text-gray-400">یه چیز برگر خفن و مردونه</p>
+            </div>
+            <div className="flex justify-start mr-4 gap-40">
+                <div className="mt-[5rem] text-orange-500 text-sm font-medium">22,500 تومان</div>
+                <div className="mt-[5rem]">2</div>
+            </div>
+        </div>
 
-        
+
         {/* <div className="bg-black">2</div>
         <div className="bg-black">3</div> */}
 
