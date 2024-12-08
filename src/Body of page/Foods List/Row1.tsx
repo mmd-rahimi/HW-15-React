20c12700564d7518dd99bf9fe431208d68599137
mmd-rahimi <mohamad8rahimi3@gmail.com>
2024-12-08 14:14:54 +0330

    created grid boxes

--- src/Body of page/Foods List/Row1.tsx
+++ src/Body of page/Foods List/Row1.tsx
@@ -1,22 +1,21 @@
 import { CounterFoodBtn } from "./CounterFoodBtn";
 
-export function Row1() {
+export function Row1({title, caption, cost}) {
     const flexFoods =
-    <div className="flex flex-row gap-5 mt-5 justify-center">
+    <div className="mx-auto">
         <div className="bg-white w-[22rem] h-[18rem] rounded-xl">
-            <div className="w-[20rem] h-[9rem] mx-auto mt-4">
+            <div className="w-[20rem] h-[9rem] mx-auto">
                 <img className="h-full w-full" src="./public/burger.jpg" alt="" />
-                <p className="font-medium text-lg mt-2">چیز برگر مرددد</p>
-                <p className="font-normal text-sm mt-2 text-gray-400">یه چیز برگر خفن و مردونه</p>
+                <p className="font-medium text-lg mt-2">{title}</p>
+                <p className="font-normal text-sm mt-2 text-gray-400">{caption}</p>
             </div>
             <div className="flex justify-start mr-4 gap-40">
-                <div className="mt-[5rem] text-orange-500 text-sm font-medium">22,500 تومان</div>
+                <div className="mt-[5rem] text-orange-500 text-sm font-medium">{cost} تومان</div>
                 <div className="mt-[5rem]">
                     <CounterFoodBtn />
                 </div>
             </div>
         </div>
-
 
         {/* <div className="bg-black">2</div>
         <div className="bg-black">3</div> */}
--- src/Root.tsx
+++ src/Root.tsx
@@ -7,7 +7,15 @@ export function Root () {
     const root = 
     <div className="bg-slate-200">
             <Header />
+            <div className="grid grid-cols-3 gap-6 mt-5 w-[58%] mx-auto">
+            <Row1 title="برگر" caption="برگر مردددد" cost="22,500"/>
             <Row1 />
+            <Row1 />
+            <Row1 />
+            <Row1 />
+            <Row1 />
+            <Row1 />
+            </div>
             <Cart />
             <Footer />
     </div>
